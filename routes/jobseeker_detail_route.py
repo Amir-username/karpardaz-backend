f2efe764aa255f79df9426a30de8d321c359f1d7
Amir <najiamirdesktop@gmail.com>
2025-04-06 18:05:46 +0330

    add jobseeker detail crud routes

---
+++ routes/jobseeker_detail_route.py
@@ -0,0 +1,82 @@
+from fastapi import APIRouter, Depends
+from sqlmodel import Session, select
+from ..models.Employer import Employer
+from ..models.JobSeekerDetail import JobSeekerDetail, JobSeekerDetailCreate, JobSeekerDetailUpdate
+from ..models.JobSeeker import JobSeeker
+from ..auth.jobseeker_auth import get_current_jobseeker
+from ..session.session import get_session
+
+
+jobseeker_detail_router = APIRouter()
+
+
+@jobseeker_detail_router.post('/jobseeker-detail/', response_model=JobSeekerDetail)
+def create_jobseeker_detail(
+    detail: JobSeekerDetailCreate,
+    jobseekr: JobSeeker = Depends(get_current_jobseeker),
+    session: Session = Depends(get_session)
+):
+    db = JobSeekerDetail.model_validate(detail)
+    db.jobseeker = jobseekr
+    db.jobseeker_id = jobseekr.id
+
+    session.add(db)
+    session.commit()
+    session.refresh(db)
+
+    return db
+
+
+@jobseeker_detail_router.get('/jobseeker-detail/{id}', response_model=JobSeekerDetail)
+def read_jobseeker_detail(
+    id: int,
+    session: Session = Depends(get_session)
+):
+    query = select(JobSeekerDetail).where(JobSeekerDetail.id == id)
+    result = session.exec(query).first()
+
+    session.add(result)
+    session.commit()
+    session.refresh(result)
+
+    return result
+
+
+@jobseeker_detail_router.patch('/jobseeker-detail/{id}', response_model=JobSeekerDetail)
+def update_jobseeker_detail(
+    id: int,
+    jobseeker_update: JobSeekerDetailUpdate,
+    jobseeker: JobSeeker = Depends(get_current_jobseeker),
+    session: Session = Depends(get_session)
+):
+    query = select(JobSeekerDetail).where(JobSeekerDetail.id == id)
+    result = session.exec(query).first()
+
+    if (result.jobseeker_id == jobseeker.id):
+        update_data = jobseeker_update.model_dump(exclude_unset=True)
+        for key, value in update_data.items():
+            setattr(result, key, value)
+
+        session.add(result)
+        session.commit()
+        session.refresh(result)
+
+    return result
+
+
+@jobseeker_detail_router.delete('/jobseeker-detail/{id}', response_model=JobSeekerDetail)
+def update_jobseeker_detail(
+    id: int,
+    jobseeker: JobSeeker = Depends(get_current_jobseeker),
+    session: Session = Depends(get_session)
+):
+    query = select(JobSeekerDetail).where(JobSeekerDetail.id == id)
+    result = session.exec(query).first()
+
+    if (jobseeker.id == result.jobseeker_id):
+        session.delete(result)
+        session.commit()
+
+        return 'deleted employer detail'
+
+    return None
