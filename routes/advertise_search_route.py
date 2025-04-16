df694ec33cd352549c4d2258358f1adcc6aa524d
Amir <najiamirdesktop@gmail.com>
2025-04-16 11:27:47 +0330

    update advertise search orderby

--- routes/advertise_search_route.py
+++ routes/advertise_search_route.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, Query
 from ..session.session import get_session
 from ..models.Advertise import Advertise, AdvertisePublic
-from sqlmodel import Session, select, or_
+from sqlmodel import Session, select, or_, desc
 
 
 search_router = APIRouter()
@@ -12,14 +12,16 @@ def search_advertises(
     session: Session = Depends(get_session),
     q: str | None = Query(None, min_length=2)
 ):
-    query = select(Advertise)
+    query = select(Advertise).order_by(
+        desc(Advertise.id))
     if q:
         query = select(Advertise).filter(
             or_(
                 Advertise.title.ilike(f"%{q}%"),
                 Advertise.description.ilike(f"%{q}%")
             )
-        )
+        ).order_by(
+            desc(Advertise.id))
 
     result = session.exec(query).all()
 
