d26d4863d66a6d3bf29e77331a1cdbf222339b88
Amir <najiamirdesktop@gmail.com>
2025-05-22 15:48:00 +0330

    add emlpoyer advertise search route

--- routes/employer/advertise_search_route.py
+++ routes/employer/advertise_search_route.py
@@ -2,6 +2,10 @@ from fastapi import APIRouter, Depends, Query
 from ...session.session import get_session
 from ...models.Advertise import Advertise, AdvertisePublic
 from sqlmodel import Session, select, or_, desc
+from ...Enums.experience_enum import ExperienceEnum
+from ...Enums.salary_enum import SalaryEnum
+from ...Enums.gender_enum import GenderEnum
+from ...Enums.position_enum import PositionEnum
 
 
 search_router = APIRouter()
@@ -10,19 +14,48 @@ search_router = APIRouter()
 @search_router.get('/jobs/search/', response_model=list[AdvertisePublic])
 def search_advertises(
     session: Session = Depends(get_session),
-    q: str | None = Query(None, min_length=2)
+    search_q: str | None = Query(None, min_length=2),
+    city_q: str | None = Query(None, min_length=2),
+    is_internship: bool | None = Query(
+        None),
+    is_remote: bool | None = Query(
+        None),
+    is_portfolio: bool | None = Query(
+        None),
+    experience: ExperienceEnum | None = Query(None),
+    salary: SalaryEnum | None = Query(None),
+    gender: GenderEnum | None = Query(None),
+    position: PositionEnum | None = Query(None)
 ):
-    query = select(Advertise).order_by(
-        desc(Advertise.id))
-    if q:
-        query = select(Advertise).filter(
+    filters = []
+    if search_q:
+        filters.append(
             or_(
-                Advertise.title.ilike(f"%{q}%"),
-                Advertise.description.ilike(f"%{q}%")
+                Advertise.title.ilike(f"%{search_q}%"),
+                Advertise.description.ilike(f"%{search_q}%"),
+                Advertise.employer.has(company_name=search_q),
             )
-        ).order_by(
-            desc(Advertise.id))
+        )
+    if city_q:
+        filters.append(Advertise.city.ilike(f"%{city_q}%"))
 
-    result = session.exec(query).all()
+    if is_internship is not None:
+        filters.append(Advertise.is_internship == is_internship)
+    if is_remote is not None:
+        filters.append(Advertise.is_remote == is_remote)
+    if is_portfolio is not None:
+        filters.append(Advertise.is_portfolio == is_portfolio)
 
+    if experience is not None:
+        filters.append(Advertise.experience == experience)
+    if salary is not None:
+        filters.append(Advertise.salary == salary)
+    if gender is not None:
+        filters.append(Advertise.gender == gender)
+    if position is not None:
+        filters.append(Advertise.position == position)
+
+    filter_query = select(Advertise).where(
+        *filters).order_by(desc(Advertise.id))
+    result = session.exec(filter_query).all()
     return result
