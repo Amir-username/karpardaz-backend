7ec7fa933b64b82b1e8ffbeb1b74d925e08ca599
amir <najiamirdesktop@gmail.com>
2025-08-18 09:57:36 +0330

    update database file

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlmodel import create_engine, SQLModel, text
 
-DATABASE_URL = "sqlite:///./karpardaz.db"
-# DATABASE_URL = "postgresql://postgres.ptpccwwupnhylniobcha:nK55Di6p354Tk1@aws-0-eu-north-1.pooler.supabase.com:6543/postgres"
+# DATABASE_URL = "sqlite:///./karpardaz.db"
+DATABASE_URL = "postgresql://postgres.ptpccwwupnhylniobcha:nK55Di6p354Tk1@aws-0-eu-north-1.pooler.supabase.com:6543/postgres"
 engine = create_engine(DATABASE_URL)
 
 
